a87151191ae96632dfd15283534c8809e1516cfe
Phil Gilligan <phil.gilligan@mandogroup.com>
2024-03-11 19:58:28 +0000

    Tidied up the step wrapper styles

--- src/components/StepWrapper/index.tsx
+++ src/components/StepWrapper/index.tsx
@@ -1,12 +1,16 @@
 import './StepWrapper.scss';
-import { StepWrapperProps } from '../../types'
+import { StepWrapperProps } from '../../types';
 
 function StepWrapper({ children, desc, title }: StepWrapperProps) {
-  return <div className="step-wrapper">
-    <h2 className="step-wrapper__title">{title}</h2>
-    <p className="step-wrapper__desc">{desc}</p>
-    {children}
-  </div>;
+  return (
+    <div className="step-wrapper">
+      <div className="step-wrapper__head">
+        <h1 className="step-wrapper__title">{title}</h1>
+        <p className="step-wrapper__desc">{desc}</p>
+      </div>
+      {children}
+    </div>
+  );
 }
 
 export default StepWrapper;
